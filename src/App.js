f2ebb4d331535a681ef30a65941f0cb429974b50
Dinsha <dinsha.jaiswal123@gmail.com>
2023-04-29 20:02:15 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+<<<<<<< HEAD
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> c747aef (Initialize project using Create React App)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
     App page
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> c747aef (Initialize project using Create React App)
     </div>
   );
 }
