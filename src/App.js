671df7cefd1ccac50965bf077acea0d4cd9058dc
Dinsha <dinsha.jaiswal123@gmail.com>
2023-04-29 20:56:59 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> c747aef (Initialize project using Create React App)
 
-function App() {
-  return (
-    <div className="App">
-<<<<<<< HEAD
-    App page
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> c747aef (Initialize project using Create React App)
-    </div>
-=======
 import {Route,Routes} from "react-router-dom";
 import HomePage from './HomePage'
 import QrGenerator from "./toolspage/QrGenerator"
@@ -39,7 +10,6 @@ function App() {
       <Route path="/qr" element={<QrGenerator/>} />
       <Route path="/invoice" element={<InvoiceGenerator/>} />
     </Routes>
->>>>>>> 5bb6e61 (changes done)
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import App from './App';
-=======
 import './index.css';
-import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
->>>>>>> c747aef (Initialize project using Create React App)
-=======
 import { BrowserRouter } from 'react-router-dom';
-import {Route,Routes} from "react-router-dom";
->>>>>>> 5bb6e61 (changes done)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,23 +13,3 @@ root.render(
 
   </React.StrictMode>
 );
-// import {BrowserRouter as Router}  from "react-router-dom";
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> c747aef (Initialize project using Create React App)
-=======
-// ReactDOM.render(
-//   <React.StrictMode>
-//   <Router>
-//     <App />
-//     </Router>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
->>>>>>> 5bb6e61 (changes done)
